9d016968a57b17b0aa5ea399e231ef3d20c79035
Raj <rajsmarty2108@gmail.com>
2022-03-16 17:34:47 +0530

    Image upload changes LINK HEROKU BACKEND UPDATED

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
-const url = "http://localhost:5000/api/items";
+const url = " https://directplacement.herokuapp.com/api/items";
 export const getItems = ()=>axios.get(`${url}/getimages`);
 export const createItem = (item)=>axios.post(`${url}/uploadimages`,item);
