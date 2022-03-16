adc689661d0407248130b24ee254ed02b3403a5b
Raj <rajsmarty2108@gmail.com>
2022-03-16 17:39:52 +0530

    Image upload changes LINK PROPERLY NOT UPDATED

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
-const url = " https://directplacement.herokuapp.com/api/items";
+const url = "https://directplacement.herokuapp.com/api/items";
 export const getItems = ()=>axios.get(`${url}/getimages`);
 export const createItem = (item)=>axios.post(`${url}/uploadimages`,item);
